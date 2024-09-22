2c701ea6011f40d7151405b36a201dab60bcaa5e
Christina-Repou <chr.repou@gmail.com>
2024-09-22 23:51:23 +0200

    add custom user model in the database

--- django/project/admin.py
+++ django/project/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Player, Room
+from django.contrib.auth.admin import UserAdmin
+from .forms import PlayerCreationForm, PlayerChangeForm
+from .models import Player
 
-admin.site.register(Player)
-admin.site.register(Room)
+class PlayerAdmin(UserAdmin):
+    add_form = PlayerCreationForm
+    form = PlayerChangeForm
+    model = Player
+    list_display = ["email", "username","wins", "losses"]
+
+admin.site.register(Player, PlayerAdmin)
--- django/project/forms.py
+++ django/project/forms.py
@@ -1,17 +1,17 @@
-from django import forms
-from .models import Player
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-class PlayerRegistrationForm(forms.ModelForm):
-	# Define any custom validation or fields if needed
-	class Meta:
-		model = Player
-		fields = ['email', 'password']
+from .models import Player
 
-	def save(self, commit=True):
-		player = super().save(commit=False)
-		# Hash the password before saving
-		player.password = make_password(self.cleaned_data['password'])
-		if commit:
-			player.save()
-		return player
+class PlayerCreationForm(UserCreationForm):
+
+    class Meta:
+        model = Player
+        fields = ("username", "email")
+
+class PlayerChangeForm(UserChangeForm):
+
+    class Meta:
+        model = Player
+        fields = ("username", "email")
+
